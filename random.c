e71dfeb926a69861f8833bedc4150abbeef6f550
Lw-Cui <645739460@QQ.com>
2015-11-07 23:35:11 +0800

    update random.c to accept command line argments

--- random.c
+++ random.c
@@ -1,14 +1,24 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define SUM 100
-#define MAX 200
+#define SUM 100000
+#define MAX 200000
 
 int main(int argc, char **argv) {
 	int i = 0;
+	int sum = SUM, max = MAX;
+	switch (argc) {
+	case 2:
+		sscanf(argv[1], "%d", &sum);
+	case 3:
+		sscanf(argv[2], "%d", &max);
+		break;
+	default:
+		break;
+	}
 
 	srand(time(NULL));
 	for (i = 0; i < SUM; i++)
-		printf("%d ", random() % MAX);
+		printf("%ld ", random() % MAX);
 	return 0;
 }
