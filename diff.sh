1563abcd460641bb4ed2d54b6e8226aba6ed2a94
Lw-Cui <645739460@QQ.com>
2015-11-07 23:41:06 +0800

    change output filename

--- diff.sh
+++ diff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./random.out > random
-./threadtree.out < random > threadtreepre
-./binarytree.out < random > binarytreepre
-diff threadtreepre binarytreepre && echo 'Yeah'
+./random.out > random.output
+./threadtree.out < random > threadtree.output
+./binarytree.out < random > binarytree.output
+diff threadtree.output binarytree.output && echo 'Yeah'
