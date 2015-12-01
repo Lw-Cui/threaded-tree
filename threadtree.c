011db7eba9e61d07b1c90afb33ddb32a7a26055f
Lw-Cui <645739460@QQ.com>
2015-12-01 12:05:44 +0800

    complement insert with thread setting

--- threadtree.c
+++ threadtree.c
@@ -19,64 +19,52 @@ struct pair *newpair(struct node *first, struct node *last) {
     return p;
 }
 
-struct node *newnode(int data, struct node *left, struct node *right) {
+struct node *newnode(int data, struct node *left) {
     struct node *n = (struct node *)malloc(sizeof(struct node));
     n->data = data;
     n->left = left;
-    n->right = right;
-    if (left == NULL)
-        n->leftnull = 1;
-    else
-        n->leftnull = 0;
+    n->right = NULL;
+	n->leftnull = 1;
     return n;
 }
 
-//TODO: ALWAYS maintain the left pointer
-struct node *insert(struct node *n, int data) {
-    if (n == NULL) {
-        return newnode(data, NULL, NULL);
-    } else if (data < n->data) {
-        if (n->left)
-            return insert(n->left, data);
-        else
-            return n->left = newnode(data, NULL, NULL);
-    } else if (data > n->data) {
-        if (n->right)
-            return insert(n->right, data);
-        else
-            return n->right = newnode(data, NULL, NULL);
-    } else {
-        return NULL;
-    }
-}
+void insert(struct node *n, int data) {
+	struct node *leftleaf;
 
-struct node *setthread(struct node *n) {
-    if (n->right == NULL && n->left == NULL) {
-        return n;
-    } else if (n->left == NULL) {
-        n->left = n->right;
-        return setthread(n->right);
-    } else if (n->right == NULL) {
-        return setthread(n->left);
-    } else {
-        setthread(n->left)->left = n->right;
-        return setthread(n->right);
-    }
+    if (data < n->data)
+        if (n->leftnull) {
+			n->leftnull = 0;
+            n->left = newnode(data, n->left);
+		} else {
+            insert(n->left, data);
+		}
+
+    if (data > n->data)
+        if (n->right) {
+            insert(n->right, data);
+		} else if (n->leftnull) {
+            n->left = n->right = newnode(data, n->left);
+		} else {
+			leftleaf = n->left;
+			while (leftleaf->left && leftleaf->left->data < n->data)
+				leftleaf = leftleaf->left;
+			leftleaf->left = n->right = newnode(data, leftleaf->left);
+		}
 }
 
 int main(int argc, char **argv) {
     int data, flag = 1;
-    struct node *root = NULL, *n;
+    struct node *root = NULL;
+	struct node *n; // Iterator
 
     while (scanf("%d", &data) == 1)
         if (flag) {
-            root = insert(root, data);
+            root = newnode(data, NULL);
             flag = 0;
         } else {
             insert(root, data);
         }
 
-    setthread(root)->left == NULL;
     printf("PREORDER\n");
     for (n = root; n != NULL; n = n->left)
         printf("%d ", n->data);
